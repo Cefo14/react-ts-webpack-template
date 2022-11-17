dea471b5edb59a5b8069cf531cf00efac1be1cce
Cefo14 <cefo_edd@hotmail.com>
2022-11-16 19:00:11 -0600

    build: move eslint and jest to config folder

--- jest.config.js
+++ jest.config.js
@@ -5,13 +5,13 @@ module.exports = {
   // A map from regular expressions to module names that allow to stub out resources, like images or styles with a single module.
   moduleNameMapper: {
     '\\.(css|scss)$': 'identity-obj-proxy',
-    '\\.(svg)$': '<rootDir>/jest/svgMapper.js',
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/jest/fileMapper.js',
+    '\\.(svg)$': '<rootDir>/config/jest/svgMapper.js',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/config/jest/fileMapper.js',
   },
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   setupFilesAfterEnv: [
-    '<rootDir>/jest/jest-setup.js'
+    '<rootDir>/config/jest/jest-setup.js'
   ],
 
   // The test environment that will be used for testing
