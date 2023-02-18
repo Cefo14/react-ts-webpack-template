9f66c16e6642536b65b226d49f6ec7f4b8be5f9a
Cefo14 <cefo_edd@hotmail.com>
2023-02-18 17:34:33 -0600

    test: change test style

--- src/app/App.spec.tsx
+++ src/app/App.spec.tsx
@@ -2,18 +2,16 @@ import { render, screen } from '@testing-library/react';
 
 import App from '.';
 
-describe('App Component', () => {
-  describe('when it has no props', () => {
-    it('should render it', () => {
-      render(
-        <App />
-      );
+describe('App', () => {
+  it('should render the app', () => {
+    render(
+      <App />
+    );
 
-      const component = screen.queryByText(/Hello World/);
-      const image = screen.queryByRole('img', { name: 'cheems' });
+    const component = screen.queryByText(/Hello World/);
+    const image = screen.queryByRole('img', { name: 'cheems' });
 
-      expect(component).toBeInTheDocument();
-      expect(image).toBeInTheDocument();
-    });
+    expect(component).toBeInTheDocument();
+    expect(image).toBeInTheDocument();
   });
 });
--- src/components/HelloWorld.spec.tsx
+++ src/components/HelloWorld.spec.tsx
@@ -3,14 +3,12 @@ import { render, screen } from '@testing-library/react';
 import HelloWorld from './HelloWorld';
 
 describe('HelloWorld Component', () => {
-  describe('when it has no props', () => {
-    it('should render it', () => {
-      render(
-        <HelloWorld />
-      );
+  it('should display the text Hello World', () => {
+    render(
+      <HelloWorld />
+    );
 
-      const component = screen.queryByText(/Hello World/);
-      expect(component).toBeInTheDocument();
-    });
+    const component = screen.getByRole('heading', { level: 1 });
+    expect(component).toHaveTextContent(/Hello World/i);
   });
 });
